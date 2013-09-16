43c0aa2c11b458c9a966ed225ff35088e79dfffe
Amadeus Folego <amadeusfolego@gmail.com>
2013-09-16 18:14:15 -0300

    Fix archives title to use config

--- lib/coil/archives_handler.ex
+++ lib/coil/archives_handler.ex
@@ -5,8 +5,9 @@ defmodule Coil.ArchivesHandler do
     template = EEx.compile_file("templates/layout.html.eex")
 
     {index_result, _} = Code.eval_quoted(index, [articles: Coil.articles])
-    {result, _} = Code.eval_quoted(template, [title: "Badosu's Archives",
-                                              content: index_result])
+    {result, _} = Code.eval_quoted(template, [
+                                   title: "#{ Coil.config("title") } - Archives",
+                                   content: index_result])
 
     {:ok, req} = :cowboy_req.reply(200, [], result, req)
     {:ok, req, state}
