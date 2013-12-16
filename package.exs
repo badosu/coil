58cdc2455f8717fe71eb0096941543f9121a996a
Amadeus Folego <amadeusfolego@gmail.com>
2013-12-16 15:13:23 -0200

    Add package.exs

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "coil", description: "Static content engine",
+                 version: "0.4.0", keywords: ["Elixir","Erlang","blog","engine","static","content"],
+                 maintainers: [[name: "Amadeus Folego", email: "amadeusfolego@gmail.com"]],
+                 repositories: [[github: "badosu/coil", tag: "v0.4.0"]])
